ddcbc423959919fdd6f60a398208f4ed11080683
E-Levine <149699489+E-Levine@users.noreply.github.com>
2023-12-06 09:33:41 -0500

    Create Repro_data_gams.R

---
+++ Repro_data_gams.R
@@ -0,0 +1,179 @@
+####Reproductive comparions of oysters in SF estuaries
+#
+#
+#Load packages, install as needed
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(plyr, tidyverse, #Df manipulation, 
+               rstatix, rcompanion, #Summary stats & multiple comparisons
+               zoo, lubridate, #Dates and times
+               readxl, writexl, #Reading excel files
+               car, #Basic analyses
+               lmPerm, lme4, 
+               lattice, ggpubr, #Plots, Arranging ggplots
+               install = TRUE)
+#
+#
+#
+#
+####Load raw data, clean, create data frames for analyses####
+#
+Repro <- read_excel("../../Data/Repro_staging.xlsx", sheet = "Raw data", #File name and sheet name
+                    skip = 0, col_names = TRUE, 
+                    col_types = c("date","text","text", "text", "text", "text", "numeric", "text", "numeric", "numeric", "text", "text", "text"), 
+                    na = c(""), trim_ws = TRUE, #Values/placeholders for NAs; trim extra white space?
+                    .name_repair = "universal") %>% rename(Site2 = Site)
+head(Repro)
+#
+Repro_df <- Repro %>% drop_na(Parasite) %>%
+  mutate_at(c("Site2", "Year", "Month", "Sex"), as.factor)  %>%
+  mutate(Site = as.factor(ifelse(grepl("CR", Site2) & Station == "1", "CR-E", 
+                                 ifelse(grepl("CR", Site2) & Station == "2", "CR-E", 
+                                        ifelse(grepl("CR", Site2) & Station == "3",  "CR-W",
+                                               ifelse(grepl("CR", Site2) & Station == "4", "CR-W", Repro$Site2)))))) %>%
+  mutate(Estuary = as.factor(ifelse(grepl("SL", Site), "SL", 
+                                    ifelse(grepl("LX", Site), "LX",
+                                           ifelse(grepl("CR", Site), "CR",
+                                                  ifelse(grepl("LW", Site), "LW", NA))))),
+         Month = factor(Month, levels = c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"))) %>%
+  mutate(Comb_Stage = ifelse(!is.na(Stage), Stage, 
+                             ifelse(Stage_Old == 0 | Stage_Old == 10, 1, 
+                                    ifelse(Stage_Old > 0 & Stage_Old < 5, 2, 
+                                           ifelse(Stage_Old > 4 & Stage_Old < 7, 3, 
+                                                  ifelse(Stage_Old > 6 & Stage_Old < 10, 4, NA)))))) %>%
+  mutate(Final_Stage = as.factor(ifelse(!is.na(SH) & Parasite == "Buceph" & is.na(Comb_Stage), 8, 
+                                        ifelse(Male_Female == "Yes", "M/F", 
+                                               ifelse(Bad_Slide == "No" & is.na(Stage) & is.na(Stage_Old), "0", Comb_Stage))))) %>%
+  dplyr::select(-Site2)
+#
+summary(Repro_df)
+#
+Data_checks <- Repro_df %>% filter(is.na(Final_Stage) & Bad_Slide != "Yes") #Dont' want any rows of data
+#rbind(Repro_df %>% filter(Sex == "M" & is.na(Final_Stage)), #Anything Bad_Slide = Yes should be NA
+#      Repro_df %>% filter(Sex == "F" & is.na(Final_Stage)))
+#
+##Write output of cleaned data (if changed)
+#write_xlsx(Repro_df, "Output/Repro_data_2023 12_cleaned.xlsx", format_headers = TRUE)
+#
+#
+##Calculate proportion per stage
+(Repro_props <- left_join(Repro_df %>% group_by(Year, Month, Estuary, Site, Final_Stage) %>% drop_na(Estuary, Final_Stage) %>%
+                            summarise(Count = n()),
+                          Repro_df %>% group_by(Year, Month, Estuary, Site) %>% drop_na(Estuary, Final_Stage) %>%
+                            summarise(Total = n())) %>%
+    mutate(Prop = Count/Total,
+           Month = as.factor(Month, levels = c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12")))) 
+#
+##Write output of cleaned data (if changed)
+write_xlsx(Repro_props, "Output/Repro_proportions_2023 12.xlsx", format_headers = TRUE)
+#
+#
+#
+####Formatting and helpers####
+#
+#Map color to Stage
+Stages <- c("0" = "Immature", "1" = "Developing", "2" = "Ripe/Spawning", "3" = "Spent/Recycling", "4" = "Indifferent", "8" = "Buceph", "M/F" = "Herm")
+cbPalette <- c("#333333", "#D55E00", "#E69F00", "#F0E442", "#009E73", "#56B4E9", "#9966FF")
+names(cbPalette) <- levels(Repro_df$Final_Stage)
+StaFill <- scale_fill_manual(name = "Stage", labels = Stages, values = cbPalette, na.value = "#999999")
+#
+##
+##
+####Data exploration####
+#
+##Stage proportion by Year, Month, Estuary, Site
+Repro_props %>% dplyr::select(-Count, -Total) %>%
+  mutate(id = row_number()) %>% gather(Variable, Value, - Final_Stage, -id, -Prop) %>%
+  ggplot(aes(Prop, Value, group = Final_Stage, color = Final_Stage))+
+  geom_point()+
+  geom_smooth(method = "lm")+
+  facet_wrap(~Variable, scales = "free_y")+ theme_classic()
+#
+Repro_props %>% dplyr::select(-Count, -Total) %>%
+  mutate(id = row_number()) %>% gather(Variable, Value, - Final_Stage, -id, -Prop) %>%
+  ggplot(aes(Value, Prop, fill = Final_Stage))+
+  geom_col(position = "fill")+
+  #geom_smooth(method = "lm")+
+  facet_wrap(~Variable, scales = "free_x")+ theme_classic()
+#
+ggsave(path = "Output/Figures/", filename = "Proportions_stages_overview.tiff", dpi = 1000, height = 5, width = 5, unit = "in")
+#
+#
+####Overall annual pattern####
+#
+##Working with all_oysters
+All_oysters_clean <- All_oysters %>% subset(Final_Stage != "M/F" & Final_Stage != "8") %>% droplevels()
+#Group by Month to compare among months 
+(Monthly_mean_counts_All <- All_oysters_clean %>% 
+    group_by(Month, Final_Stage) %>% 
+    count() %>%
+    summarize(meanCount = mean(n),
+              minCount = min(n),
+              maxCount = max(n)) %>%
+    pivot_wider(id_cols = Month,
+                names_from = Final_Stage,
+                values_from = c("meanCount", "minCount", "maxCount"), 
+                names_glue = "{Final_Stage}_{.value}"))
+#  
+All_oysters_clean %>% group_by(Month) %>% 
+  ggplot(aes(Month, fill = Final_Stage))+
+  geom_bar(position = "fill")+
+  scale_y_continuous("Percentage", labels = scales::percent_format(), expand = c(0,0))+
+  StaFill
+#
+##pearson chi-squared
+(All_test <- chisq.test(All_oysters_clean$Final_Stage, All_oysters_clean$Month))
+(All_test_pvalues <- chisq.posthoc.test::chisq.posthoc.test(xtabs(~Final_Stage + Month, All_oysters_clean), method = "bonferroni"))
+#
+##KW test
+kruskal.test(as.numeric(Final_Stage) ~ Month, data = All_oysters_clean)
+pairwise.wilcox.test(as.numeric(All_oysters_clean$Final_Stage), All_oysters_clean$Month, p.adjust.method = "BH")
+#
+#
+##Site differences
+(Site_mean_counts_All <- All_oysters_clean %>% 
+    group_by(Site, Final_Stage) %>% 
+    count() %>%
+    summarize(meanCount = mean(n),
+              minCount = min(n),
+              maxCount = max(n)) %>%
+    pivot_wider(id_cols = Site,
+                names_from = Final_Stage,
+                values_from = c("meanCount", "minCount", "maxCount"), 
+                names_glue = "{Final_Stage}_{.value}"))
+#
+All_oysters_clean %>% group_by(Site) %>% 
+  ggplot(aes(Site, fill = Final_Stage))+
+  geom_bar(position = "fill")+
+  scale_y_continuous("Percentage", labels = scales::percent_format(), expand = c(0,0))+
+  StaFill
+#
+##KW test
+kruskal.test(as.numeric(Final_Stage) ~ Site, data = All_oysters_clean)
+pairwise.wilcox.test(as.numeric(All_oysters_clean$Final_Stage), All_oysters_clean$Site, p.adjust.method = "BH")
+#
+#
+#
+#Group by Month and Site to compare among months and sites
+(Monthly_mean_counts_All_Sites <- All_oysters_clean %>%
+    group_by(Month, Site, Final_Stage) %>% 
+    count() %>% #ungroup() %>%
+    summarize(meanCount = mean(n),
+              minCount = min(n),
+              maxCount = max(n)) %>%
+    pivot_wider(id_cols = c("Month", "Site"),
+                names_from = Final_Stage,
+                values_from = c("meanCount", "minCount", "maxCount"), 
+                names_glue = "{Final_Stage}_{.value}") %>%
+    arrange(Site))
+#  
+All_oysters_clean %>% group_by(Month, Site) %>% 
+  ggplot(aes(Month, fill = Final_Stage))+
+  geom_bar(position = "fill")+
+  lemon::facet_rep_wrap(.~Site)+
+  scale_y_continuous("Percentage", labels = scales::percent_format(), expand = c(0,0))+
+  StaFill
+#
+All_oysters_clean %>% group_by(Month) %>% 
+  group_by(Month, Final_Stage) %>% 
+  count() %>% #ungroup() %>%
+  summarize(medianCount = median(n)) 
