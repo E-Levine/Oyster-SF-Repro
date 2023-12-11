5b2e2eaa825591954c91a9aca0bebe1f9297ffaa
E-Levine <149699489+E-Levine@users.noreply.github.com>
2023-12-11 11:13:01 -0500

    Updated for one file for data cleaning. Save output for use in any anlyses. All ZZ slides with no Buceph chanegd to Bad slide" for exclusion.

--- Repro_data_summaries.R
+++ Repro_data_summaries.R
@@ -21,24 +21,33 @@ Repro <- read_excel("../../Data/Repro_staging.xlsx", sheet = "Raw data", #File n
                     skip = 0, col_names = TRUE, 
                     col_types = c("date","text","text", "text", "text", "text", "numeric", "text", "numeric", "numeric", "text", "text", "text"), 
                     na = c(""), trim_ws = TRUE, #Values/placeholders for NAs; trim extra white space?
-                    .name_repair = "universal")
+                    .name_repair = "universal") %>% rename(Site2 = Site)
 head(Repro)
 #
 Repro_df <- Repro %>% drop_na(Parasite) %>%
-  mutate_at(c("Site", "Year", "Month", "Sex"), as.factor) %>%
+  mutate_at(c("Site2", "Year", "Month", "Sex"), as.factor)  %>%
+  mutate(Site = as.factor(ifelse(grepl("CR", Site2) & Station == "1", "CR-E", 
+                                 ifelse(grepl("CR", Site2) & Station == "2", "CR-E", 
+                                        ifelse(grepl("CR", Site2) & Station == "3",  "CR-W",
+                                               ifelse(grepl("CR", Site2) & Station == "4", "CR-W", Repro$Site2)))))) %>%
   mutate(Estuary = as.factor(ifelse(grepl("SL", Site), "SL", 
                                     ifelse(grepl("LX", Site), "LX",
                                            ifelse(grepl("CR", Site), "CR",
                                                   ifelse(grepl("LW", Site), "LW", NA))))),
-         Month = factor(Month, levels = c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12")))%>%
+         Month = factor(Month, levels = c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"))) %>%
   mutate(Comb_Stage = ifelse(!is.na(Stage), Stage, 
-                             ifelse(Stage_Old == 0 | Stage_Old == 10, 1, 
-                                    ifelse(Stage_Old > 0 & Stage_Old < 5, 2, 
-                                           ifelse(Stage_Old > 4 & Stage_Old < 7, 3, 
-                                                  ifelse(Stage_Old > 6 & Stage_Old < 10, 4, NA)))))) %>%
+                             ifelse(Stage_Old == 0 | Stage_Old == 10, 4, 
+                                    ifelse(Stage_Old > 0 & Stage_Old < 5, 1, 
+                                           ifelse(Stage_Old > 4 & Stage_Old < 8, 2, 
+                                                  ifelse(Stage_Old > 7 & Stage_Old < 10, 3, NA)))))) %>%
   mutate(Final_Stage = as.factor(ifelse(!is.na(SH) & Parasite == "Buceph" & is.na(Comb_Stage), 8, 
-                              ifelse(Male_Female == "Yes", "M/F", 
-                                     ifelse(Bad_Slide == "No" & is.na(Stage) & is.na(Stage_Old), "0", Comb_Stage)))))
+                                        ifelse(Male_Female == "Yes", "M/F", 
+                                               ifelse(Bad_Slide == "No" & is.na(Stage) & is.na(Stage_Old), "0", Comb_Stage)))),
+         Buceph = as.factor(ifelse(Parasite == "Buceph", "Y", "N"))) %>%
+  dplyr::select(-Site2)
+#
+temp <- Repro %>% filter(is.na(Stage) & Male_Female == "No" & is.na(Stage_Old) & Parasite != "Buceph" & Bad_Slide == "No")
+#
 #
 summary(Repro_df)
 #
@@ -47,7 +56,33 @@ Data_checks <- Repro_df %>% filter(is.na(Final_Stage) & Bad_Slide != "Yes") #Don
   #      Repro_df %>% filter(Sex == "F" & is.na(Final_Stage)))
 #
 ##Write output of cleaned data
-write_xlsx(Repro_df, "Output/Repro_data_2023 12_cleaned.xlsx", format_headers = TRUE)
+#write_xlsx(Repro_df, "Output/Repro_data_2023 12 06_cleaned.xlsx", format_headers = TRUE)
+#
+#
+##Calculate proportion per stage
+(Repro_props <- left_join(Repro_df %>% group_by(Year, Month, Estuary, Site, Final_Stage) %>% drop_na(Estuary, Final_Stage) %>%
+                            summarise(Count = n()),
+                          Repro_df %>% group_by(Year, Month, Estuary, Site) %>% drop_na(Estuary, Final_Stage) %>%
+                            summarise(Total = n())) %>%
+    mutate(Prop = Count/Total)) 
+#
+##Write output of cleaned data (if changed)
+#write_xlsx(Repro_props, "Output/Repro_proportions_2023 12 06.xlsx", format_headers = TRUE)
+#
+summary(Repro_props)
+#
+#
+####Formatting and helpers####
+#
+#Map color to Stage
+Stages <- c("0" = "Immature", "1" = "Developing", "2" = "Ripe/Spawning", "3" = "Spent/Recycling", "4" = "Indifferent", "8" = "Buceph", "M/F" = "Herm")
+cbPalette <- c("#333333", "#D55E00", "#E69F00", "#F0E442", "#009E73", "#56B4E9", "#9966FF")
+names(cbPalette) <- levels(Repro_df$Final_Stage)
+StaFill <- scale_fill_manual(name = "Stage", labels = Stages, values = cbPalette, na.value = "#999999")
+#
+##
+###
+####By estuary####
 #
 #Dataframes by Estuary - counts of each stage
 SL_oysters <- Repro_df %>% subset(Estuary == "SL") 
@@ -56,13 +91,29 @@ LW_oysters <- Repro_df %>% subset(Site == "LW")
 CR_oysters <- Repro_df %>% subset(Estuary == "CR") %>% mutate(Site = as.factor(ifelse(Station == "1" | Station == "2", "CR-E", "CR-W"))) 
 All_oysters <- rbind(SL_oysters, LX_oysters, LW_oysters, CR_oysters)
 #
-write_xlsx(All_oysters, "Output/Repro_data_2023 12_cleaned_final.xlsx", format_headers = TRUE)
+#write_xlsx(All_oysters, "Output/Repro_data_2023 12_cleaned_final.xlsx", format_headers = TRUE)
 #
 #
-Stages <- c("0" = "Immature", "1" = "Developing", "2" = "Ripe/Spawning", "3" = "Spent/Recycling", "4" = "Indifferent", "8" = "Buceph", "M/F" = "Herm")
-cbPalette <- c("#333333", "#D55E00", "#E69F00", "#F0E442", "#009E73", "#56B4E9", "#9966FF")
-names(cbPalette) <- levels(Repro_df$Final_Stage)
-StaFill <- scale_fill_manual(name = "Stage", labels = Stages, values = cbPalette, na.value = "#999999")
+#
+#
+####Data checks####
+#
+#
+#Comparing old stages
+ggarrange(Repro_props_annual %>%
+            ggplot(aes(Year, Prop, fill = Final_Stage))+
+            geom_col(position = "fill")+ StaFill+
+            theme_classic(),
+          left_join(Repro_df %>% group_by(Year, Month, Estuary, Site, Stage_Old) %>% drop_na(Estuary) %>%
+                      summarise(Count = n()),
+                    Repro_df %>% group_by(Year) %>% drop_na(Estuary) %>%
+                      summarise(Total = n())) %>%
+            mutate(Prop = Count/Total) %>%
+            ggplot(aes(Year, Prop, fill = as.factor(Stage_Old)))+
+            geom_col(position = "fill") + theme_classic())
+#
+#
+#
 #
 #
 ####Overall annual pattern####
