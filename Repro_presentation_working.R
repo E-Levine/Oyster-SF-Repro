b91e4f62505c4aed9f37b9eb235c7aa245be4525
E-Levine <149699489+E-Levine@users.noreply.github.com>
2024-02-07 09:42:49 -0500

    Updates.
    Updated code and saved output for L50 curve of maturity-all samples.

--- Repro_presentation_working.R
+++ Repro_presentation_working.R
@@ -48,6 +48,12 @@ cbPalette <- c("#666666", "#D55E00", "#E69F00", "#F0E442", "#009E73", "#56B4E9",
 names(cbPalette) <- levels(Repro_df$Final_Stage)
 StaFill <- scale_fill_manual(name = "Stage", labels = Stages, values = cbPalette, na.value = "#999999")
 #
+##Color to sex
+Sex <- c("F" = "Female", "M" = "Male", "M/F" = "M/F", "Z" = "Undetermined")
+color_og <- c("#009E73", "#E69F00", "#9966FF", "#666666")
+#Map color to Sex
+names(color_og) <- c("F","M","M/F","Z")
+MFCol <- scale_color_manual(name = "", labels = Sex, values = color_og) 
 #
 Base <- theme_bw() +
   theme(panel.grid = element_blank(), panel.border = element_blank(), panel.background = element_blank(),
@@ -58,6 +64,12 @@ Base <- theme_bw() +
         axis.text.y = element_text(size = 13, color = "black", 
                                    margin = unit(c(0, 0.4, 0, 0), "cm"), family = "sans"),
         axis.ticks.length = unit(-0.15, "cm"), plot.margin = margin(0.25, 0.5, 0.25, 0.25, "cm"))
+#
+theme_f <- theme(strip.text.y = element_text(color = "black", size = 11, family = "sans", face = "bold"),
+                 strip.background = element_rect(fill = "#CCCCCC"),
+                 panel.spacing = unit(0.75, "lines"),
+                 strip.text.x = element_text(size = 10, face = "bold", family = "sans"))
+#
 #
 ##
 ###
@@ -112,3 +124,40 @@ Maturity %>% subset(Mature == "I") %>% summarise(n())
 #
 ##Already see a slight issue.
 ##Based on 46 samples staged as immature:
+#
+##Working from scratch:
+round5 <- function(x){5*ceiling(x/5)} #Rounds x up to nearest 5
+#Limited to desrired columns and check for missing data
+mat_all <- Maturity %>%  mutate(Mature = as.factor(Mature), Sex = as.factor(Sex)) %>% 
+  dplyr::select(SH, bins, Sex, Mature) %>% drop_na()
+Amelia::missmap(mat_all) #None missing
+#
+#Fit model
+lrSH <- glm(Mature ~ SH, family = binomial(link = "logit"), data = mat_all)
+output <- data.frame(SH = seq(0, round5(max(mat_all$SH)), 5))
+output$Mature <- predict(lrSH, newdata = output, type = "response")
+#Get x where y = 0.5
+(LD50 <- MASS::dose.p(lrSH, p = 0.5))
+summary(lrSH)
+#
+mat_all %>%
+  ggplot(aes(SH, as.numeric(Mature)-1))+
+  geom_jitter(aes(color = Sex), size = 3, alpha = 0.6, width = 0.1, height = 0.05)+
+  stat_smooth(method = "glm", se = FALSE, fullrange = TRUE, 
+              method.args = list(family = binomial), size = 1.25)+
+  Base + MFCol +
+  geom_vline(xintercept = LD50[[1]],linetype = "dashed", color = "black", size = 1)+
+  scale_x_continuous(name = "Shell height (mm)", expand = c(0,0), limits = c(0, round5(max(mat_all$SH))), breaks = seq(0, round5(max(mat_all$SH)), by = 10))+
+  scale_y_continuous(name = "Proportion mature", expand = c(0.025,0.025), limits = c(0,1))
+
+#
+#
+#Clearly isn't informative. What next? Attempt to use "indifferent" as immature?
+Maturity %>% filter(Mature == "M") %>% get_summary_stats(SH, type = "five_number")
+#
+Maturity %>% filter(Final_Stage == "1" & SH < 47) %>%
+  ggplot(aes(SH))+
+  geom_histogram(aes(y = ..count..), breaks = seq(0,90, by = 5))+
+  Base + scale_x_continuous("Shell height (mm)", expand = c(0,0)) + scale_y_continuous("Count", expand = c(0,0))
+#
+#
